d6f0e3e264f8ac1c354f606dbf3f024f683ac7ce
Youssif Hany <youssifhany962@gmail.com>
2022-02-25 14:13:08 +0200

    Initial Commit

---
+++ RoadAssistant/RoadAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadAssistantApp.swift
+//  RoadAssistant
+//
+//  Created by Youssif Hany on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RoadAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
