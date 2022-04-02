479c80b2747f51c84944093048ee083ee8ed59b2
Youssif Hany <youssifhany962@gmail.com>
2022-04-02 02:07:54 +0200

    Data Service

--- RoadAssistant/DataServices/LocationsDataService.swift
+++ RoadAssistant/DataServices/LocationsDataService.swift
@@ -16,7 +16,6 @@ class LocationsDataService {
             roadName: "26th july axis road",
             cityName: "6 October city",
             coordinates: CLLocationCoordinate2D(latitude:30.00908 , longitude:30.98528),
-            //description: "Pothole in the middle of the road.",
             imageName: [
              "pothole1"
             ]
@@ -26,7 +25,6 @@ class LocationsDataService {
             roadName: "Waslet Dahshur Road",
             cityName: "Sheikh Zayed City",
             coordinates: CLLocationCoordinate2D(latitude:30.01520 , longitude:30.97636),
-           // description: "Pothole in the rigth side of the road.",
             imageName: [
              "pothole2"
             ]
@@ -36,7 +34,6 @@ class LocationsDataService {
             roadName: "Gamal Abd Al Nasser Street",
             cityName: "6 October city",
             coordinates: CLLocationCoordinate2D(latitude:30.00620 , longitude:30.96839),
-           // description: "Pothole in the middle of the road.",
             imageName: [
              "pothole3"
             ]
@@ -46,7 +43,6 @@ class LocationsDataService {
             roadName: "Al Wahat Road",
             cityName: "6 October city",
             coordinates: CLLocationCoordinate2D(latitude:29.97262 , longitude:31.00669),
-          //  description: "Pothole in the left side of the road.",
             imageName: [
              "pothole4"
             ]
@@ -56,7 +52,6 @@ class LocationsDataService {
             roadName: "26th july axis Rd",
             cityName: "6 October city",
             coordinates: CLLocationCoordinate2D(latitude:30.00866 , longitude:30.99187),
-          //  description: "Cracks in the left side of the road.",
             imageName: [
              "crack1"
             ]
@@ -66,7 +61,6 @@ class LocationsDataService {
             roadName: "Waslet Dahshur Rd",
             cityName: "Sheikh Zayed City",
             coordinates: CLLocationCoordinate2D(latitude:30.02976 , longitude:30.96388),
-          //  description: "Cracks in the rigth side of the road.",
             imageName: [
              "crack2"
             ]
@@ -76,7 +70,6 @@ class LocationsDataService {
             roadName: "Gamal Abd Al Nasser St",
             cityName: "6 October city",
             coordinates: CLLocationCoordinate2D(latitude:30.00379 , longitude:30.96491),
-      //      description: "Cracks in the middle of the road.",
             imageName: [
              "crack3"
             ]
@@ -86,7 +79,6 @@ class LocationsDataService {
             roadName: "Al Wahat Rd",
             cityName: "6 October city",
             coordinates: CLLocationCoordinate2D(latitude:29.96919 , longitude:31.01779),
-   //         description: "Cracks in the right side of the road.",
             imageName: [
              "crack4"
             ]
