47a6134a2124966bec789fcb31db579561e33cf5
Youssif Hany <youssifhany962@gmail.com>
2022-06-06 00:47:29 +0200

    Fake DATA

--- RoadAssistant/DataServices/LocationsDataService.swift
+++ RoadAssistant/DataServices/LocationsDataService.swift
@@ -21,68 +21,68 @@ class LocationsDataService {
             ]
         ),
         
-        Defect(
-            roadName: "Waslet Dahshur Road",
-            cityName: "Sheikh Zayed City",
-            coordinates: CLLocationCoordinate2D(latitude:30.01520 , longitude:30.97636),
-            imageName: [
-             "pothole2"
-            ]
-        ),
-        
-        Defect(
-            roadName: "Gamal Abd Al Nasser Street",
-            cityName: "6 October city",
-            coordinates: CLLocationCoordinate2D(latitude:30.00620 , longitude:30.96839),
-            imageName: [
-             "pothole3"
-            ]
-        ),
-        
-        Defect(
-            roadName: "Al Wahat Road",
-            cityName: "6 October city",
-            coordinates: CLLocationCoordinate2D(latitude:29.97262 , longitude:31.00669),
-            imageName: [
-             "pothole4"
-            ]
-        ),
-        
-        Defect(
-            roadName: "26th july axis Rd",
-            cityName: "6 October city",
-            coordinates: CLLocationCoordinate2D(latitude:30.00866 , longitude:30.99187),
-            imageName: [
-             "crack1"
-            ]
-        ),
-        
-        Defect(
-            roadName: "Waslet Dahshur Rd",
-            cityName: "Sheikh Zayed City",
-            coordinates: CLLocationCoordinate2D(latitude:30.02976 , longitude:30.96388),
-            imageName: [
-             "crack2"
-            ]
-        ),
-        
-        Defect(
-            roadName: "Gamal Abd Al Nasser St",
-            cityName: "6 October city",
-            coordinates: CLLocationCoordinate2D(latitude:30.00379 , longitude:30.96491),
-            imageName: [
-             "crack3"
-            ]
-        ),
-        
-        Defect(
-            roadName: "Al Wahat Rd",
-            cityName: "6 October city",
-            coordinates: CLLocationCoordinate2D(latitude:29.96919 , longitude:31.01779),
-            imageName: [
-             "crack4"
-            ]
-        ),
+//        Defect(
+//            roadName: "Waslet Dahshur Road",
+//            cityName: "Sheikh Zayed City",
+//            coordinates: CLLocationCoordinate2D(latitude:30.01520 , longitude:30.97636),
+//            imageName: [
+//             "pothole2"
+//            ]
+//        ),
+//
+//        Defect(
+//            roadName: "Gamal Abd Al Nasser Street",
+//            cityName: "6 October city",
+//            coordinates: CLLocationCoordinate2D(latitude:30.00620 , longitude:30.96839),
+//            imageName: [
+//             "pothole3"
+//            ]
+//        ),
+//
+//        Defect(
+//            roadName: "Al Wahat Road",
+//            cityName: "6 October city",
+//            coordinates: CLLocationCoordinate2D(latitude:29.97262 , longitude:31.00669),
+//            imageName: [
+//             "pothole4"
+//            ]
+//        ),
+//
+//        Defect(
+//            roadName: "26th july axis Rd",
+//            cityName: "6 October city",
+//            coordinates: CLLocationCoordinate2D(latitude:30.00866 , longitude:30.99187),
+//            imageName: [
+//             "crack1"
+//            ]
+//        ),
+//
+//        Defect(
+//            roadName: "Waslet Dahshur Rd",
+//            cityName: "Sheikh Zayed City",
+//            coordinates: CLLocationCoordinate2D(latitude:30.02976 , longitude:30.96388),
+//            imageName: [
+//             "crack2"
+//            ]
+//        ),
+//
+//        Defect(
+//            roadName: "Gamal Abd Al Nasser St",
+//            cityName: "6 October city",
+//            coordinates: CLLocationCoordinate2D(latitude:30.00379 , longitude:30.96491),
+//            imageName: [
+//             "crack3"
+//            ]
+//        ),
+//
+//        Defect(
+//            roadName: "Al Wahat Rd",
+//            cityName: "6 October city",
+//            coordinates: CLLocationCoordinate2D(latitude:29.96919 , longitude:31.01779),
+//            imageName: [
+//             "crack4"
+//            ]
+//        ),
     ]
     
 }
