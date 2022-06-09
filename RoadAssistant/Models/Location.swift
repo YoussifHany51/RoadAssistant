337dc389b1a3ec5a78dadfcfabca8c3fdf5f0611
Youssif Hany <youssifhany962@gmail.com>
2022-06-09 17:46:53 +0200

    Decodable model

--- RoadAssistant/Models/Location.swift
+++ RoadAssistant/Models/Location.swift
@@ -13,38 +13,34 @@ struct Defect : Identifiable , Equatable, Codable{
         
     var id = UUID().uuidString
     var roadName : String = ""
-    var cityName : String = ""
     var coordinates : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude:0 , longitude:0)
     var imageName : [String] = []
     
     private enum CodingKeys: String, CodingKey {
-        case roadName,cityName,coordinates,imageName
+        case roadName,coordinates,imageName,latitude,longitude
     }
     
     //Equatable
     static func == (lhs: Defect, rhs: Defect) -> Bool {
         lhs.id == rhs.id
     }
-    init(roadName:String,cityName:String,coordinates:CLLocationCoordinate2D,imageName:[String]) {
+    init(roadName:String,coordinates:CLLocationCoordinate2D,imageName:[String]) {
         
         self.roadName = roadName
-        self.cityName = cityName
         self.coordinates = coordinates
         self.imageName = imageName
     }
     
     
     init(from decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let values = try decoder.container(keyedBy: CodingKeys.self)
 //        id = try values.decode(String.self,forKey: .id)
-//        roadName = try values.decode(String.self,forKey: .roadName)
-//        cityName = try values.decode(String.self,forKey: .cityName)
-//        coordinates = try values.decode(CLLocationCoordinate2D.self,forKey: .coordinates)
-//        imageName = try values.decode([String].self,forKey: .imageName)
-        
-        
+        roadName = try values.decode(String.self,forKey: .roadName)
+        coordinates.latitude = CLLocationDegrees(try values.decode(Double.self,forKey: .latitude))
+        coordinates.longitude = CLLocationDegrees(try values.decode(Double.self,forKey: .longitude))
+        imageName = try values.decode([String].self,forKey: .imageName)
     }
-    
+  
     
     func encode(to encoder: Encoder) throws {
         
