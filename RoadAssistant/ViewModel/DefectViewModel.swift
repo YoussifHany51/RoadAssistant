a7dd19a8c028c9221f66b5218d42695e7360dced
Youssif Hany <youssifhany962@gmail.com>
2022-06-09 17:45:44 +0200

    Push and retrieve Data from firebase

--- RoadAssistant/ViewModel/DefectViewModel.swift
+++ RoadAssistant/ViewModel/DefectViewModel.swift
@@ -13,6 +13,9 @@ import Firebase
 import UIKit
 import FirebaseDatabase
 import FirebaseDatabaseSwift
+import CodableFirebase
+import FirebaseFirestore
+//import SwiftProtobuf
 
 class DefectViewModel: ObservableObject {
     //MARK: Camera & Photo picker
@@ -23,24 +26,22 @@ class DefectViewModel: ObservableObject {
     @Published var cameraError: Picker.CameraErrorType?
     
     @Published var defectRoadName: String = ""
-    @Published var defectCityName: String = ""
     @Published var defectLocation: CLLocationCoordinate2D =
     CLLocationCoordinate2D(latitude:0 , longitude:0)
     @Published var isEditing = false
     @Published var showFileAlert = false
     @Published var reportLocationBTPressed = false
     @Published var appError: MyImageError.ErrorType?
-    
+    @Published var fetchData = [Defect]()
     
     func reset(){
         image = nil
         defectRoadName = ""
-        defectCityName = ""
     }
     
     
     var userReportValidation: Bool{
-        image == nil || defectRoadName.isEmpty || defectCityName.isEmpty || reportLocationBTPressed == false
+        image == nil || defectRoadName.isEmpty || reportLocationBTPressed == false
     }
     
     
@@ -59,7 +60,7 @@ class DefectViewModel: ObservableObject {
     
     // MARK: LOCATION VIEW MODEL
     
-    @Published var defects: [Defect]
+    @Published var defects = [Defect]()
     
     
     @Published var mapLocation : Defect {
@@ -85,6 +86,7 @@ class DefectViewModel: ObservableObject {
         self.defects = defects
         self.mapLocation = defects.first!
         self.updateMapRegion(defect: defects.first!)
+        fetchDefects()
     }
     
     private func updateMapRegion(defect:Defect){
@@ -123,43 +125,82 @@ class DefectViewModel: ObservableObject {
         nextLocation(defect: nextLoc)
     }
     //   MARK: Firebase DB
-  
+    
     func convertUIImageToString (image: UIImage) -> [String] {
-
+        
         var imageAsData: Data = image.pngData()!
         var imageAsInt: Int = 0 // initialize
-
+        
         let data = NSData(bytes: &imageAsData, length: MemoryLayout<Int>.size)
         data.getBytes(&imageAsInt, length: MemoryLayout<Int>.size)
-
+        
         let imageAsString: String = String (imageAsInt)
         
         var res:[String] = []
         res.append(imageAsString)
-
+        
         return res
-
-    }
-    
-    private let ref = Database.database().reference()
-    private var number = 0
-    
-    
-    func pushReport(roadName:String,cityName:String,location:CLLocationCoordinate2D,image:UIImage){
-    let img = convertUIImageToString(image: image)
-        var defectObj = Defect(id: String(number), roadName: roadName, cityName: cityName, coordinates: location, imageName: img)
-
-//        defectObj.id = String(number)
-//        defectObj.roadName = roadName
-//        defectObj.cityName = cityName
-//        defectObj.coordinates = location
-//        defectObj.imageName = img
         
-        ref.child(defectObj.id).setValue(defectObj.toDictionary)
+    }
+    var ref : DatabaseReference! = Database.database().reference()
+    var number = 0;
+    func pushReport(roadName:String,coordinates:CLLocationCoordinate2D,image:UIImage){
         
-        number+=1
+        let img = convertUIImageToString(image: image)
+        let latitude = coordinates.latitude
+        let longitude = coordinates.longitude
+//        let defectObj = Defect(roadName: roadName, coordinates: coordinates, imageName: img)
+        
+//        let defectDictionary:[String:Any] = [
+//            "roadName" : defectObj.roadName,
+//            "coordinates" : defectObj.coordinates,
+//            "imageName" : defectObj.imageName] as [String : Any]
+        
+//        ref.child("DefectInfo").childByAutoId().setValue(defectDictionary.description,
+//                                                         withCompletionBlock: {error ,ref in
+//            if let error = error {
+//                print("DefectInfoDictionary was not saved: \(error.localizedDescription)")
+//            }else{
+//                print("DefectInfoDictionary saved successfully!")
+//                self.number+=1
+//            }
+//        })
+
+        self.ref.child("Defect").childByAutoId().setValue(
+            ["roadName": roadName,"latitude": latitude,"longitude":longitude,"imageName": img])
+        
     }
-    func pushTest(){
-        ref.setValue("Test")
-    }
+
+    
+    
+    func fetchDefects(){
+        self.ref.child("Defect").observe(.value) { snapshot in
+            guard let value = snapshot.value else {return}
+            
+            print(snapshot.key)
+            print(value)
+            
+            do{
+                let model = try FirebaseDecoder().decode([String:Defect].self, from: value)
+                print(model)
+                let arrayOfKeys = Array(model.keys.map{ $0 })
+                print(arrayOfKeys) // [1, 2, 3]
+                let arrayOfValues = Array(model.values.map{ $0 })
+                print(arrayOfValues)
+                
+                for item in arrayOfValues{
+                    print(item)
+                    self.defects.append(item)
+                }
+            } catch let error {
+                print(error)
+            }
+        }withCancel: { error in
+            print(error.localizedDescription)
+        }
+        
+  }
+    
+    
+    
 }
