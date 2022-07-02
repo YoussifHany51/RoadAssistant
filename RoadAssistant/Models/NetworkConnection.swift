6b2c6b19e330f8867d787a2b22d67ec04ac6cf2f
Youssif Hany <youssifhany962@gmail.com>
2022-07-02 19:06:26 +0200

    Network checker

--- RoadAssistant/Models/NetworkConnection.swift
+++ RoadAssistant/Models/NetworkConnection.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import Network
+
+final class NetworkConnection: ObservableObject{
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "Monitor")
+    
+    @Published var isConnected = true
+    
+    init(){
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async{
+                self?.isConnected = path.status == .satisfied ? true : false
+            }
+        }
+        monitor.start(queue: queue)
+    }
+}
