75d0d33a677916893b2752cfb7f91d0a20167ce0
Youssif Hany <youssifhany962@gmail.com>
2022-07-02 19:20:58 +0200

    loading View

--- RoadAssistant/Views/LoadingView.swift
+++ RoadAssistant/Views/LoadingView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State var animate = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Circle()
+                .trim(from: 0, to: 0.8)
+                .stroke(AngularGradient(gradient:
+                        .init(colors:
+                                [.red,.orange]),center: .center),
+                        style: StrokeStyle(lineWidth:8,
+                                           lineCap: .round))
+                .frame(width:45,height: 45)
+                .rotationEffect(.init(degrees: self.animate ? 360 : 0))
+                .animation(Animation.linear(duration: 0.7).repeatForever(), value: animate)
+        }
+        .background(Color.white)
+        .cornerRadius(15)
+        .onAppear{
+            self.animate.toggle()
+        }
     }
 }
 
