a48c446ce5c241aef670440526246de3f5b1fcd7
Youssif Hany <youssifhany962@gmail.com>
2022-07-02 19:10:26 +0200

    User report View enhancing

--- RoadAssistant/Views/CameraViews/UserReportView.swift
+++ RoadAssistant/Views/CameraViews/UserReportView.swift
@@ -9,82 +9,98 @@ import SwiftUI
 import MapKit
 import CoreLocationUI
 
+extension View{
+    func dismissKeyBoard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),to: nil,from: nil,for: nil)
+    }
+}
+
+
 struct UserReportView: View {
     
-    @EnvironmentObject var vm : LocationViewModel
+    @EnvironmentObject var vm : DefectViewModel
+    @StateObject var viewModel = DefectViewModel()
     @ObservedObject var locationManager = LocationManager.shared
     
     @State var reportAlert: Bool = false
+    @State var defectLocationAlert: Bool = false
     @State var showSheet: Bool = false
     @State var userLocationExplanation: Bool = false
- 
     
     var body: some View {
         NavigationView {
-                VStack{
-                    header
-                    .sheet(isPresented: $showSheet, content: {
-                        InfoView()
-                    })
-                    
-                    
-                    if let image = vm.image {
-                        ZoomableScrollView {
-                            Image(uiImage: image)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(minWidth:0,maxWidth: .infinity)
-                        }
-                    }else{
-                        Image(systemName: "photo.fill")
+            VStack{
+                header
+//                    .sheet(isPresented: $showSheet, content: {
+//                        InfoView()
+//                    })
+                
+                
+                if let image = vm.image {
+                    ZoomableScrollView {
+                        Image(uiImage: image)
                             .resizable()
                             .scaledToFit()
-                            .opacity(0.6)
                             .frame(minWidth:0,maxWidth: .infinity)
-                            .padding(.horizontal)
                     }
-                    VStack{
-                        
-                        
-                            defectTextFields
+                }else{
+                    Image(systemName: "photo.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(0.6)
+                        .frame(minWidth:0,maxWidth: .infinity)
+                        .padding(.horizontal)
+                }
+                VStack{
+                    
+                    
+                    defectTextFields
+                        .padding()
+                    
+                    VStack {
+                        currentLocation
                             .padding()
+                            .alert(isPresented: $defectLocationAlert, content: {
+                                Alert(
+                                    title: Text("Location Saved !")
+                                )
+                            })
                         
-                        VStack {
-                            currentLocation
-                            .padding()
-                            
-                            reportButton
-        
+                        reportButton
+                        
                             .alert(isPresented: $reportAlert, content: {
                                 Alert(
                                     title: Text("Report saved ✅"),
                                     message: Text("Thank you 😘")
                                 )
-                        })
-                        }
-                        cameraOptionsButtons
-                        .padding()
+                            })
                     }
-                    Spacer()
+                    cameraOptionsButtons
+                        .padding()
                 }
-                .sheet(isPresented: $vm.showPicker){
-                    ImagePicker(sourceType: vm.source == .library ? .photoLibrary : .camera, selectedImage: $vm.image)
-                        .ignoresSafeArea()
+                .onTapGesture {
+                    self.dismissKeyBoard()
                 }
-                .alert("Error", isPresented: $vm.showFileAlert, presenting: vm.appError,actions:{ cameraError in
-                    cameraError.button
-                }, message:{ cameraError in
-                    Text(cameraError.message)
-                })
-            .navigationBarHidden(true)
+                Spacer()
             }
+            .sheet(isPresented: $vm.showPicker){
+                ImagePicker(sourceType: vm.source == .library ? .photoLibrary : .camera, selectedImage: $vm.image)
+                    .ignoresSafeArea()
+            }
+            .alert("Error", isPresented: $vm.showFileAlert, presenting: vm.appError,actions:{ cameraError in
+                cameraError.button
+            }, message:{ cameraError in
+                Text(cameraError.message)
+            })
+            .navigationBarHidden(true)
         }
+    }
 }
 
 struct CameraView_Previews: PreviewProvider {
     static var previews: some View {
         UserReportView()
-            .environmentObject(LocationViewModel())
+            .environmentObject(DefectViewModel())
     }
 }
 
@@ -103,8 +119,8 @@ extension UserReportView{
             }, message:{ cameraError in
                 Text(cameraError.message)
             })
-
-
+            
+            
             Button{
                 vm.source = .library
                 vm.showPhotoPicker()
@@ -121,78 +137,66 @@ extension UserReportView{
                 .font(.title)
                 .padding()
             
-            Button
-            {
-                showSheet.toggle()
-            }label:{
-                Image(systemName:"info.circle")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:30,height: 30)
-                    .padding()
-            }
+//            Button
+//            {
+//                showSheet.toggle()
+//            }label:{
+//                Image(systemName:"info.circle")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width:30,height: 30)
+//                    .padding()
+//            }
         }
     }
+    //MARK: Text Field
     private var defectTextFields : some View{
         VStack{
             
             TextField("Defect Road Name", text: $vm.defectRoadName){isEditing in
                 vm.isEditing = isEditing
-                    
+                
             }
-            .padding()
-            TextField("Defect City Name", text: $vm.defectCityName){isEditing in
-                vm.isEditing = isEditing
-                    
-            }
-            .padding()
+            .padding(.horizontal)
+            .frame(height:55)
+            .background(Color.gray.opacity(0.3))
+            .cornerRadius(10)
+            .font(.title)
         }
-        .textFieldStyle(.roundedBorder)
-        .font(.title2)
         
     }
     //MARK: REPORT LOCATION
     private var currentLocation: some View{
-//        Button{
-//
-//        }label: {
-//        Text("Use Current Location")
-//                .font(.headline)
-//                .padding()
-//                .frame(height:40)
-//                .background(Color.blue)
-//                .foregroundColor(.white)
-//                .cornerRadius(15)
-//        }
-
-        LocationButton(){
+        Button{
             vm.reportLocationBTPressed.toggle()
             LocationManager.shared.requstLocation()
             
-       if let locationCoordinate = locationManager.userLocation?.coordinate{
+            
+            if let locationCoordinate = locationManager.userLocation?.coordinate{
+                vm.defectLocation = locationCoordinate
+                defectLocationAlert.toggle()
                 print(locationCoordinate)
             }
+            else {
+                print("Location Extraction Faild")
+            }
+        }label: {
+            Text("Defect Location")
+                .font(.callout)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .clipShape(Capsule())
         }
-        .font(.body)
-        .foregroundColor(.white)
-        .cornerRadius(8)
-//        .onLongPressGesture{
-//            userLocationExplanation.toggle()
-//        }
-//        .alert(isPresented: $userLocationExplanation, content: {
-//            Alert(
-//                title: Text("Current Location Button Explanation"),
-//                message: Text("This button allows us to get the defect specific coordinates")
-//            )
-//        })
         
     }
     
-    //MARK: SUBMIT REPORT BT
+    //MARK: REPORT Button
     private var reportButton: some View{
         HStack {
             Button{
-                
+                viewModel.pushReport(roadName:vm.defectRoadName,coordinates: vm.defectLocation)
+                viewModel.uploadDefectImage(image: vm.image!)
                 SoundManager.instance.playSound()
                 vm.reset()
                 reportAlert.toggle()
